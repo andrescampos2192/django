82d80c0c34eb70b9db38ff67d2105e73eb5dd9c7
andrescampos2192 <64428496+andrescampos2192@users.noreply.github.com>
2025-04-09 11:47:08 -0400

    push changes

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect  # No olvides importar redirect
 from .models import task
 from .forms import taskform
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 def task_list_and_create(request):
     if request.method == 'POST':
@@ -17,3 +20,13 @@ def task_list_and_create(request):
         'form': form,
         'tasks': tasks
     })
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # o al nombre correcto de tu vista de login
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
